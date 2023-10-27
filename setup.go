f29cc5e9a6a230b8f13147c1c6f802d58a3e922f
samar2170 <scipioresearch2020@gmail.com>
2023-10-27 19:57:49 +0530

    log dir setup

--- setup.go
+++ setup.go
@@ -21,10 +21,29 @@ func setup() {
 		}
 	}
 	createUploadDir()
+	createLogDir()
 }
 
 func createUploadDir() {
 	err := os.Mkdir(UploadDir, 0755)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createLogDir() {
+	fds, err := os.ReadDir(".")
+	if err != nil {
+		panic(err)
+	}
+	for _, fd := range fds {
+		if fd.IsDir() {
+			if fd.Name() == "logs" {
+				return
+			}
+		}
+	}
+	err = os.Mkdir("logs", 0755)
 	if err != nil {
 		panic(err)
 	}
